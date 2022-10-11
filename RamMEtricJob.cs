63372efa93deccb8d7d706b1649c7e69a9671023
Timma1991 <97384175+Timma1991@users.noreply.github.com>
2022-10-11 14:01:59 +0300

    Add files via upload

---
+++ RamMEtricJob.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using MetricsAgent.Services.Impl;
+using Quartz;
+
+namespace MetricsAgent.Job
+{
+    public class RamMEtricJob
+    { 
+    private PerformanceCounter _ramCounter;
+    private IServiceScopeFactory _serviceScopeFactory;
+
+        public RamMEtricJob(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+            _ramCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+    }
+
+
+    public Task Execute(IJobExecutionContext context)
+    {
+
+        using (IServiceScope serviceScope = _serviceScopeFactory.CreateScope())
+        {
+            var ramMetricsRepository = serviceScope.ServiceProvider.GetService<RamMetricRepository>();
+            try
+            {
+                var cpuUsageInPercents = _ramCounter.NextValue();
+                var time = TimeSpan.FromSeconds(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+                Debug.WriteLine($"{time} > {cpuUsageInPercents}");
+                ramMetricsRepository.Create(new Models.RamMertic
+                {
+                    Value = (int)cpuUsageInPercents,
+                    Time = (long)time.TotalSeconds
+                });
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
+}
---
+++ SturtUpcs.cs
@@ -0,0 +1,6 @@
+﻿namespace MetricsAgent
+{
+    public class SturtUpcs
+    {
+    }
+}
